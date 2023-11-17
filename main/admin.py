17713b667586af68bd74164ae1557206b958c3c1
Andrii <andreyyerzhov@gmail.com>
2023-11-17 13:43:13 +0200

    feat: add models and register them

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import System, Unit, StructuralRisk, FunctionalRisk, FailureProbability, Damage
+
+admin.site.register(System)
+admin.site.register(Unit)
+admin.site.register(StructuralRisk)
+admin.site.register(FunctionalRisk)
+admin.site.register(FailureProbability)
+admin.site.register(Damage)
+
--- main/models.py
+++ main/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
+
 # Create your models here.
+class System(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    # ?
+    #operating_mode = models.CharField(choices=[('Non-Regular', 'Non-Regular'), ('Regular', 'Regular')],
+    #                                  verbose_name='Operating Mode')
+    #duration_of_operation = models.IntegerField()
+    #cost = models.DecimalField()
+
+    system = models.ForeignKey(System, on_delete=models.CASCADE)
+
+
+class StructuralRisk(models.Model):
+    value = models.FloatField()
+    output = models.CharField(max_length=20)
+
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+
+class FunctionalRisk(models.Model):
+    value = models.FloatField()
+    output = models.CharField(max_length=20)
+
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+
+class FailureProbability(models.Model):
+    value = models.FloatField()
+    output = models.CharField(max_length=20)
+
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+
+class Damage(models.Model):
+    value = models.FloatField()
+    output = models.CharField(max_length=20)
+
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+
+# class ExtraOption(models.Model):
+#     operatingMode
+#     duration of operation
+#     cost
