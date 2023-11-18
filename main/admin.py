6dfca6ec06c0d22712a3ad803cbab8c542d8b56a
Andrii <andreyyerzhov@gmail.com>
2023-11-19 00:24:16 +0200

    feat: register new models in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
-from main.models import System, Unit, StructuralRisk, FunctionalRisk, FailureProbability, Damage
+from main.models import System, Unit, StructuralRisk, FunctionalRisk, FailureProbability, Damage, OperatingMode, \
+    DurationOfOperation, Rules
 
 admin.site.register(System)
 admin.site.register(Unit)
+admin.site.register(OperatingMode)
+admin.site.register(DurationOfOperation)
 admin.site.register(StructuralRisk)
 admin.site.register(FunctionalRisk)
 admin.site.register(FailureProbability)
 admin.site.register(Damage)
+admin.site.register(Rules)
 
